157272fff0d4f355fe8d0a487cebcaa277990346
Motaphe <suzalregmii@gmail.com>
2023-05-18 13:45:53 -0400

    added discord presence

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  "andweeb/presence.nvim",
+  config = function()
+    require("presence").setup {}
+  end,
+}
+
+
