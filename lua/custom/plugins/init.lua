76a9cd565ccd1bab076a2d586425bf24f089c3d7
Motaphe <suzalregmii@gmail.com>
2025-10-21 17:05:04 -0400

    Replace tokyonight with Catppuccin theme.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -24,4 +24,31 @@ return {
       },
     },
   },
+
+  -- Catppuccin Theme
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      flavour = 'mocha', -- latte, frappe, macchiato, mocha
+      transparent_background = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        mini = {
+          enabled = true,
+        },
+        telescope = {
+          enabled = true,
+        },
+      },
+    },
+    -- Activate catppuccin as the colorscheme
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
 }
